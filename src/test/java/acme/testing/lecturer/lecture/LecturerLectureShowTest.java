3079377db64fb155b5079580703c1e3a783a82c5
Francisco Benítez <frabenrui1@alum.us.es>
2023-05-19 17:18:53 +0200

    Show Lecture test

---
+++ src/test/java/acme/testing/lecturer/lecture/LecturerLectureShowTest.java
@@ -0,0 +1,70 @@
+
+package acme.testing.lecturer.lecture;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.lectures.Lecture;
+import acme.testing.TestHarness;
+
+public class LecturerLectureShowTest extends TestHarness {
+
+	@Autowired
+	protected LecturerLectureRepositoryTest repository;
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/lecture/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int recordIndex, final String title, final String summary, final String estimatedLearningTime, final String body, final String lectureType, final String link) {
+		super.signIn("lecturer1", "lecturer1");
+
+		super.clickOnMenu("Lecturer", "My lectures");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("summary", summary);
+		super.checkInputBoxHasValue("estimatedLearningTime", estimatedLearningTime);
+		super.checkInputBoxHasValue("body", body);
+		super.checkInputBoxHasValue("lectureType", lectureType);
+		super.checkInputBoxHasValue("link", link);
+
+		super.signOut();
+
+	}
+
+	@Test
+	public void test300Hacking() {
+		// HINT: this test tries to create a Lecture using principals with
+		// HINT+ inappropriate roles.
+
+		Collection<Lecture> lectures;
+		String param;
+
+		lectures = this.repository.findLecturesByLecturer("lecturer1");
+		for (final Lecture lecture : lectures) {
+			param = String.format("id=%d", lecture.getId());
+
+			super.checkLinkExists("Sign in");
+			super.request("/lecturer/lecture/show", param);
+			super.checkPanicExists();
+
+			super.signIn("administrator1", "administrator1");
+			super.request("/lecturer/lecture/show", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("lecturer2", "lecturer2");
+			super.request("/lecturer/lecture/show", param);
+			super.checkPanicExists();
+			super.signOut();
+		}
+
+	}
+}
