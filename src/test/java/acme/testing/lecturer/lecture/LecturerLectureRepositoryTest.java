39a2e2af2785493f434a6d50aab7c4b706204912
Francisco Benítez <frabenrui1@alum.us.es>
2023-05-19 18:01:11 +0200

    List lectures in my course test

--- src/test/java/acme/testing/lecturer/lecture/LecturerLectureRepositoryTest.java
+++ src/test/java/acme/testing/lecturer/lecture/LecturerLectureRepositoryTest.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import org.springframework.data.jpa.repository.Query;
 
+import acme.entities.courses.Course;
 import acme.entities.lectures.Lecture;
 import acme.framework.repositories.AbstractRepository;
 
@@ -12,4 +13,7 @@ public interface LecturerLectureRepositoryTest extends AbstractRepository {
 
 	@Query("select l from Lecture l where l.lecturer.userAccount.username = :lecturer")
 	Collection<Lecture> findLecturesByLecturer(String lecturer);
+
+	@Query("select c from Course c where c.lecturer.userAccount.username = :lecturer")
+	Collection<Course> findCoursesByLecturer(String lecturer);
 }
