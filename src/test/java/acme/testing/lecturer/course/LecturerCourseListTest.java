86165415f55471b866925654d0e0d7c7d3a1ee2b
Francisco Benítez <frabenrui1@alum.us.es>
2023-05-20 19:52:23 +0200

    List Course test finished

---
+++ src/test/java/acme/testing/lecturer/course/LecturerCourseListTest.java
@@ -0,0 +1,52 @@
+
+package acme.testing.lecturer.course;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.testing.TestHarness;
+import acme.testing.lecturer.lecture.LecturerLectureRepositoryTest;
+
+public class LecturerCourseListTest extends TestHarness {
+
+	@Autowired
+	protected LecturerLectureRepositoryTest repository;
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/course/list-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int recordIndex, final String code, final String title, final String price) {
+
+		super.signIn("lecturer1", "lecturer1");
+
+		super.clickOnMenu("Lecturer", "My courses");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, code);
+		super.checkColumnHasValue(recordIndex, 1, title);
+		super.checkColumnHasValue(recordIndex, 2, price);
+
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+		super.checkLinkExists("Sign in");
+		super.request("/lecturer/course/list");
+		super.checkPanicExists();
+
+		super.signIn("administrator1", "administrator1");
+		super.request("/lecturer/course/list");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("student2", "student2");
+		super.request("/lecturer/course/list");
+		super.checkPanicExists();
+		super.signOut();
+	}
+
+}
