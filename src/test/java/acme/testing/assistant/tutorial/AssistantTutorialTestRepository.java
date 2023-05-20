65975ba030564bad8f5deacacf31d9bf73d6b6f5
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-05-20 17:23:10 +0200

    primer test

--- src/test/java/acme/testing/assistant/tutorial/AssistantTutorialTestRepository.java
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialTestRepository.java
@@ -1,8 +1,16 @@
 
 package acme.testing.assistant.tutorial;
 
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+
+import acme.entities.tutorials.Tutorial;
 import acme.framework.repositories.AbstractRepository;
 
 public interface AssistantTutorialTestRepository extends AbstractRepository {
+
+	@Query("select t from Tutorial t where t.assistant.userAccount.username = :assistant")
+	Collection<Tutorial> findTutorialsByAssistant(String assistant);
 
 }
