116bd359a20710a543e100dd34824e7b8c00f81b
Francisco Benítez <frabenrui1@alum.us.es>
2023-05-21 22:10:59 +0200

    LectureCourse tests finished

--- src/test/java/acme/testing/lecturer/lectureCourse/LecturerLectureCourseListTest.java
+++ src/test/java/acme/testing/lecturer/lectureCourse/LecturerLectureCourseListTest.java
@@ -1,5 +1,5 @@
 
-package acme.testing.lecturer.lecture;
+package acme.testing.lecturer.lectureCourse;
 
 import java.util.Collection;
 
@@ -11,31 +11,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import acme.entities.courses.Course;
 import acme.testing.TestHarness;
 
-public class LecturerLectureListTest extends TestHarness {
+public class LecturerLectureCourseListTest extends TestHarness {
 
 	@Autowired
-	protected LecturerLectureRepositoryTest repository;
+	protected LecturerLectureCourseRepositoryTest repository;
 
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/lecturer/lecture/list-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive(final int recordIndex, final String title, final String estimatedLearningTime, final String lectureType) {
-
+	@CsvFileSource(resources = "/lecturer/lecture-course/list-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int courseIndex, final int recordIndex, final String title, final String estimatedLearningTime, final String lectureType) {
 		super.signIn("lecturer1", "lecturer1");
 
 		super.clickOnMenu("Lecturer", "My courses");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 
-		super.clickOnListingRecord(0);
-		super.checkFormExists();
+		super.clickOnListingRecord(courseIndex);
 		super.clickOnButton("See lectures");
 
+		super.checkListingExists();
+		super.sortListing(0, "asc");
 		super.checkColumnHasValue(recordIndex, 0, title);
 		super.checkColumnHasValue(recordIndex, 1, estimatedLearningTime);
 		super.checkColumnHasValue(recordIndex, 2, lectureType);
 
 		super.signOut();
+
 	}
 
 	@Test
@@ -48,19 +49,18 @@ public class LecturerLectureListTest extends TestHarness {
 			param = String.format("courseId=%d", course.getId());
 
 			super.checkLinkExists("Sign in");
-			super.request("/lecturer/lecture/list", param);
+			super.request("/lecturer/lecture-course/list", param);
 			super.checkPanicExists();
 
 			super.signIn("administrator1", "administrator1");
-			super.request("/lecturer/lecture/list", param);
+			super.request("/lecturer/lecture-course/list", param);
 			super.checkPanicExists();
 			super.signOut();
 
 			super.signIn("lecturer2", "lecturer2");
-			super.request("/lecturer/lecture/list", param);
+			super.request("/lecturer/lecture-course/list", param);
 			super.checkPanicExists();
 			super.signOut();
 		}
 	}
-
 }
