b11a0242ba706534deb7a0171f63ea0effcdebf2
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-05-23 18:47:00 +0200

    test de update tutorial

--- src/test/java/acme/testing/assistant/tutorial/AssistantTutorialUpdateTest.java
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialUpdateTest.java
@@ -4,6 +4,8 @@ package acme.testing.assistant.tutorial;
 import java.util.Collection;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.entities.tutorials.Tutorial;
@@ -14,63 +16,59 @@ public class AssistantTutorialUpdateTest extends TestHarness {
 	@Autowired
 	protected AssistantTutorialTestRepository repository;
 
-	//	@ParameterizedTest
-	//	@CsvFileSource(resources = "/assistant/tutorial/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	//	public void test100Positive(final int recordIndex, final String code, final String title, final String summary, final String goals, final boolean draftMode, final String estimatedTotalTime, final String course) {
-	//		super.signIn("assistant1", "assistant1");
-	//
-	//		super.clickOnMenu("Assistant", "Tutorials List");
-	//		super.checkListingExists();
-	//		super.sortListing(0, "asc");
-	//
-	//		super.clickOnListingRecord(recordIndex);
-	//		super.checkFormExists();
-	//		super.fillInputBoxIn("title", title);
-	//		super.fillInputBoxIn("summary", summary);
-	//		super.fillInputBoxIn("estimatedLearningTime", estimatedLearningTime);
-	//		super.fillInputBoxIn("body", body);
-	//		super.fillInputBoxIn("lectureType", lectureType);
-	//		super.fillInputBoxIn("link", link);
-	//		super.clickOnSubmit("Update");
-	//
-	//		super.checkListingExists();
-	//		super.sortListing(0, "asc");
-	//		super.clickOnListingRecord(recordIndex);
-	//		super.checkFormExists();
-	//		super.checkInputBoxHasValue("title", title);
-	//		super.checkInputBoxHasValue("summary", summary);
-	//		super.checkInputBoxHasValue("estimatedLearningTime", estimatedLearningTime);
-	//		super.checkInputBoxHasValue("body", body);
-	//		super.checkInputBoxHasValue("lectureType", lectureType);
-	//		super.checkInputBoxHasValue("link", link);
-	//
-	//		super.signOut();
-	//	}
 
-	//	@ParameterizedTest
-	//	@CsvFileSource(resources = "/assistant/tutorial/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	//	public void test200Negative(final int recordIndex, final String code, final String title, final String summary, final String goals, final boolean draftMode, final String estimatedTotalTime, final String course) {
-	//		super.signIn("assistant1", "assistant1");
-	//
-	//		super.clickOnMenu("Assistant", "Tutorials List");
-	//		super.checkListingExists();
-	//		super.sortListing(0, "asc");
-	//
-	//		super.clickOnListingRecord(recordIndex);
-	//		super.checkFormExists();
-	//		super.fillInputBoxIn("title", title);
-	//		super.fillInputBoxIn("summary", summary);
-	//		super.fillInputBoxIn("estimatedLearningTime", estimatedLearningTime);
-	//		super.fillInputBoxIn("body", body);
-	//		super.fillInputBoxIn("lectureType", lectureType);
-	//		super.fillInputBoxIn("link", link);
-	//		super.clickOnSubmit("Update");
-	//
-	//		super.checkErrorsExist();
-	//
-	//		super.signOut();
-	//	}
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int recordIndex, final String code, final String title, final String summary, final String goals, final boolean draftMode, final String estimatedTotalTime, final String course) {
+		super.signIn("assistant1", "assistant1");
 
+		super.clickOnMenu("Assistant", "Tutorials List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(recordIndex);
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("summary", summary);
+		super.fillInputBoxIn("goals", goals);
+		super.fillInputBoxIn("course", course);
+		super.clickOnSubmit("Update");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("summary", summary);
+		super.fillInputBoxIn("goals", goals);
+		super.fillInputBoxIn("course", course);
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int recordIndex, final String code, final String title, final String summary, final String goals, final boolean draftMode, final String estimatedTotalTime, final String course) {
+		super.signIn("assistant1", "assistant1");
+
+		super.clickOnMenu("Assistant", "Tutorials List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("summary", summary);
+		super.fillInputBoxIn("goals", goals);
+		super.fillInputBoxIn("course", course);
+		super.clickOnSubmit("Update");
+
+		super.checkErrorsExist();
+
+		super.signOut();
+	}
 
 	@Test
 	public void test300Hacking() {
