2453f9aef4a4e8126fcd5c774eeb83fe55712bb1
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2023-06-19 13:04:47 +0200

    correccion de errores

---
+++ src/main/java/acme/features/assistant/dashboard/AssistantDashboardRepository.java
@@ -0,0 +1,11 @@
+
+package acme.features.assistant.dashboard;
+
+import org.springframework.stereotype.Repository;
+
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AssistantDashboardRepository extends AbstractRepository {
+
+}
